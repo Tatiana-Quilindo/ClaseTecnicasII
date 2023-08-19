28c8cd2bf0c091654a53ca4373e19d4c4ec7bd7d
Tatiana-Quilindo <126364394+Tatiana-Quilindo@users.noreply.github.com>
2023-08-19 10:04:16 -0500

    Primera esructura del proyecto.

--- TecnicasProgramaciónII/TecnicasProgramaciónII/Program.cs
+++ TecnicasProgramaciónII/TecnicasProgramaciónII/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
         }
+
     }
 }
